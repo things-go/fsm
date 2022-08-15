140e45e37cb45d334bbb8dbd637048ebb8e109a8
thinkgo <thinkgo@aliyun.com>
2022-08-15 19:06:35 +0800

    fix: fix fsm

--- fsm.go
+++ fsm.go
@@ -10,7 +10,12 @@ import (
 // E ist the event
 // S is the state
 type FSM[E constraints.Ordered, S constraints.Ordered] struct {
-	*FSMUnsafe[E, S]
+	// current is the state that the FSM is currently in.
+	current S
+
+	// Translator contain events and source states to destination states.
+	// This is immutable
+	Translator[E, S]
 
 	// stateMu guards access to the current state.
 	stateMu sync.RWMutex
@@ -20,9 +25,7 @@ type FSM[E constraints.Ordered, S constraints.Ordered] struct {
 // E is the event type
 // S is the state type.
 func New[E constraints.Ordered, S constraints.Ordered](initState S, transitions Transforms[E, S]) *FSM[E, S] {
-	return &FSM[E, S]{
-		FSMUnsafe: NewUnsafeFSM[E, S](initState, transitions),
-	}
+	return NewFromTransitions[E, S](initState, NewTranslator[E, S](transitions))
 }
 
 // NewFromTransitions constructs a generic FSM with an initial state S, for events E.
@@ -30,21 +33,24 @@ func New[E constraints.Ordered, S constraints.Ordered](initState S, transitions 
 // S is the state type.
 func NewFromTransitions[E constraints.Ordered, S constraints.Ordered](initState S, ts Translator[E, S]) *FSM[E, S] {
 	return &FSM[E, S]{
-		FSMUnsafe: NewUnsafeFSMFromTransitions(initState, ts),
+		current:    initState,
+		Translator: ts,
 	}
 }
 
 // Clone the FSM without Mutex.
 func (f *FSM[E, S]) Clone() *FSM[E, S] {
 	return &FSM[E, S]{
-		FSMUnsafe: f.FSMUnsafe.Clone(),
+		current:    f.current,
+		Translator: f.Translator,
 	}
 }
 
 // CloneWithState clone the FSM with new state without Mutex.
 func (f *FSM[E, S]) CloneWithState(newState S) *FSM[E, S] {
 	return &FSM[E, S]{
-		FSMUnsafe: f.FSMUnsafe.CloneWithState(newState),
+		current:    newState,
+		Translator: f.Translator,
 	}
 }
 
@@ -52,21 +58,21 @@ func (f *FSM[E, S]) CloneWithState(newState S) *FSM[E, S] {
 func (f *FSM[E, S]) Current() S {
 	f.stateMu.RLock()
 	defer f.stateMu.RUnlock()
-	return f.FSMUnsafe.Current()
+	return f.current
 }
 
 // Is returns true if state is the current state.
 func (f *FSM[E, S]) Is(state S) bool {
 	f.stateMu.RLock()
 	defer f.stateMu.RUnlock()
-	return f.FSMUnsafe.Is(state)
+	return state == f.current
 }
 
 // SetState allows the user to move to the given state from current state.
 func (f *FSM[E, S]) SetState(state S) {
 	f.stateMu.Lock()
 	defer f.stateMu.Unlock()
-	f.FSMUnsafe.SetState(state)
+	f.current = state
 }
 
 // Trigger call a state transition with the named event.
@@ -77,7 +83,12 @@ func (f *FSM[E, S]) SetState(state S) {
 func (f *FSM[E, S]) Trigger(event E) error {
 	f.stateMu.Lock()
 	defer f.stateMu.Unlock()
-	return f.FSMUnsafe.Trigger(event)
+	dst, err := f.Translator.Trigger(f.current, event)
+	if err != nil {
+		return err
+	}
+	f.current = dst
+	return nil
 }
 
 // ShouldTrigger return dst state transition with the named event and src state.
@@ -85,5 +96,21 @@ func (f *FSM[E, S]) Trigger(event E) error {
 func (f *FSM[E, S]) ShouldTrigger(event E) {
 	f.stateMu.Lock()
 	defer f.stateMu.Unlock()
-	f.FSMUnsafe.ShouldTrigger(event)
+	f.current = f.Translator.ShouldTrigger(f.current, event)
+}
+
+// IsCan returns true if event can occur in the current state.
+func (f *FSM[E, S]) IsCan(event E) bool {
+	return f.Translator.IsCan(f.current, event)
+}
+
+// IsAllCan returns true if all the events can occur in src state.
+func (f *FSM[E, S]) IsAllCan(event ...E) bool {
+	return f.Translator.IsAllCan(f.current, event...)
+}
+
+// AvailTransitionEvent returns a list of available transition event in the
+// current state.
+func (f *FSM[E, S]) AvailTransitionEvent() []E {
+	return f.Translator.AvailTransitionEvent(f.current)
 }
--- fsm_unsafe_test.go
+++ fsm_unsafe_test.go
@@ -2,13 +2,34 @@ package fsm
 
 import (
 	"testing"
+
+	"golang.org/x/exp/slices"
 )
+
+func TestUnsafeClone(t *testing.T) {
+	ts := NewTranslator(Transforms[string, string]{
+		{Event: "open", Src: []string{"closed"}, Dst: "open"},
+		{Event: "close", Src: []string{"open"}, Dst: "closed"},
+	})
+
+	fsm := NewUnsafeFSMFromTransitions("close", ts)
+	fsm1 := fsm.Clone()
+	if fsm1.Current() != fsm.Current() {
+		t.Errorf("expected same current state")
+	}
+
+	fsm2 := fsm.CloneWithState("open")
+	if fsm2.Current() != "open" {
+		t.Error("expected state to be 'open'")
+	}
+}
 
 func TestUnsafeState(t *testing.T) {
 	fsm := NewUnsafeFSM(
 		"walking",
 		Transforms[string, string]{
 			{Event: "walk", Src: []string{"start"}, Dst: "walking"},
+			{Event: "look", Src: []string{"walking"}, Dst: "walking"},
 		},
 	)
 
@@ -17,6 +38,12 @@ func TestUnsafeState(t *testing.T) {
 	}
 	if fsm.ContainEvent("nosupport") {
 		t.Error("expected not support event 'nosupport'")
+	}
+	if !fsm.ContainAllEvent("walk", "look") {
+		t.Error("expected support all event 'walk' and 'look'")
+	}
+	if fsm.ContainAllEvent("walk", "nosupport") {
+		t.Error("expected support all event 'walk' and 'nosupport'")
 	}
 
 	fsm.SetState("start")
@@ -35,6 +62,26 @@ func TestUnsafeState(t *testing.T) {
 	}
 }
 
+func TestUnsafeAvailTransitionEvent(t *testing.T) {
+	fsm := NewUnsafeFSM(
+		"closed",
+		Transforms[string, string]{
+			{Event: "open", Src: []string{"closed"}, Dst: "open"},
+			{Event: "close", Src: []string{"open"}, Dst: "closed"},
+			{Event: "middle", Src: []string{"closed"}, Dst: "middle"},
+		},
+	)
+	events := fsm.AvailTransitionEvent()
+	if !(slices.Contains(events, "middle") && slices.Contains(events, "open")) {
+		t.Error("expected contain [middle, open] event with current state")
+	}
+	if !fsm.IsAllCan("middle", "open") {
+		t.Error("expected contain all [middle, open] event with current state")
+	}
+	if fsm.IsAllCan("open", "close") {
+		t.Error("expected not contain all [middle, open] event with current state")
+	}
+}
 func TestUnsafeInappropriateEvent(t *testing.T) {
 	fsm := NewUnsafeFSM(
 		"closed",
@@ -47,6 +94,11 @@ func TestUnsafeInappropriateEvent(t *testing.T) {
 	err := fsm.Trigger("close")
 	if err != ErrInappropriateEvent {
 		t.Error("expected 'ErrInappropriateEvent' with correct state and event")
+	}
+	historyState := fsm.Current()
+	fsm.ShouldTrigger("close")
+	if historyState != fsm.Current() {
+		t.Error("ShouldTrigger expected hold original state with correct event")
 	}
 }
 
@@ -61,6 +113,11 @@ func TestUnsafeNonExistEvent(t *testing.T) {
 
 	err := fsm.Trigger("lock")
 	if err != ErrNonExistEvent {
-		t.Error("expected 'ErrNonExistEvent' with correct event")
+		t.Error("expected 'ErrNonExistEvent' with incorrect event")
+	}
+	historyState := fsm.Current()
+	fsm.ShouldTrigger("lock")
+	if historyState != fsm.Current() {
+		t.Error("ShouldTrigger expected hold original state with incorrect event")
 	}
 }
