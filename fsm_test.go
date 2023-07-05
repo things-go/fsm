b8327488dee0a7c21f78b37cde52b29b3ced49e6
thinkgos <thinkgo@aliyun.com>
2023-07-05 15:50:29 +0800

    fix: fix test

--- fsm_test.go
+++ fsm_test.go
@@ -9,6 +9,22 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+type LampEvent string
+
+const (
+	LampEvent_Open         LampEvent = "open"
+	LampEvent_Close        LampEvent = "close"
+	LampEvent_PartialClose LampEvent = "partial-close"
+	LampEvent_PartialOpen  LampEvent = "partial-open"
+	LampEvent_Look         LampEvent = "look"
+)
+
+func (l LampEvent) String() string { return string(l) }
+
+func formatEvent[E fmt.Stringer](e E) string {
+	return fmt.Sprintf("<%s>", e.String())
+}
+
 type LampStatus string
 
 const (
@@ -19,201 +35,216 @@ const (
 
 func (l LampStatus) String() string { return string(l) }
 
-type LampEvent string
-
-const (
-	LampEvent_Open         LampEvent = "open"
-	LampEvent_Close        LampEvent = "close"
-	LampEvent_PartialClose LampEvent = "partial-close"
-	LampEvent_PartialOpen  LampEvent = "partial-open"
-)
-
-func (l LampEvent) String() string { return string(l) }
-
-func formatEvent[E fmt.Stringer](e E) string {
-	return fmt.Sprintf("<%s>", e.String())
-}
-
 func formatState[S fmt.Stringer](e S) string {
 	return fmt.Sprintf(">%s", e.String())
 }
 
-func Test_Fsm_Clone(t *testing.T) {
-	test_Fsm_Clone(t, NewSafeFsm[string, string])
-	test_Fsm_Clone(t, NewFsm[string, string])
+type testTranslatorError struct{}
+
+func (testTranslatorError) Translate(err error) error {
+	switch err {
+	case ErrInappropriateEvent:
+		return errors.New("err1")
+	case ErrNonExistEvent:
+		return errors.New("err2")
+	}
+	return err
 }
 
-func test_Fsm_Clone(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
-	ts := NewTransition([]Transform[string, string]{
-		{Event: "open", Src: []string{"closed"}, Dst: "open"},
-		{Event: "close", Src: []string{"open"}, Dst: "closed"},
-	})
+func Test_Fsm_Clone(t *testing.T) {
+	test_Fsm_Clone(t, NewSafeFsm[LampEvent, LampStatus])
+	test_Fsm_Clone(t, NewFsm[LampEvent, LampStatus])
+}
 
-	fsm := newFsm("close", ts)
+func test_Fsm_Clone(t *testing.T, newFsm func(initState LampStatus, ts ITransition[LampEvent, LampStatus]) IFsm[LampEvent, LampStatus]) {
+	fsm := newFsm(
+		LampStatus_Closed,
+		NewTransition([]Transform[LampEvent, LampStatus]{
+			{Event: LampEvent_Open, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Opened},
+			{Event: LampEvent_Close, Src: []LampStatus{LampStatus_Opened}, Dst: LampStatus_Closed},
+		}),
+	)
 	fsm1 := fsm.Clone()
 	if fsm1.Current() != fsm.Current() {
 		t.Errorf("expected same current state")
 	}
-	fsm2 := fsm.CloneNewState("open")
-	if fsm2.Current() != "open" {
-		t.Error("expected state to be 'open'")
+	fsm2 := fsm.CloneNewState(LampStatus_Opened)
+	if fsm2.Current() != LampStatus_Opened {
+		t.Error("expected state to be 'opened'")
 	}
 }
 
 func Test_Fsm_SameState(t *testing.T) {
-	test_Fsm_SameState(t, NewSafeFsm[string, string])
-	test_Fsm_SameState(t, NewFsm[string, string])
+	test_Fsm_SameState(t, NewSafeFsm[LampEvent, LampStatus])
+	test_Fsm_SameState(t, NewFsm[LampEvent, LampStatus])
 }
 
-func test_Fsm_SameState(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
+func test_Fsm_SameState(t *testing.T, newFsm func(initState LampStatus, ts ITransition[LampEvent, LampStatus]) IFsm[LampEvent, LampStatus]) {
 	fsm := newFsm(
-		"start",
-		NewTransition([]Transform[string, string]{
-			{Event: "run", Src: []string{"start"}, Dst: "start"},
+		LampStatus_Closed,
+		NewTransition([]Transform[LampEvent, LampStatus]{
+			{Event: LampEvent_Close, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Closed},
 		}),
 	)
-	err := fsm.Trigger("run")
+	err := fsm.Trigger(LampEvent_Close)
 	if err != nil {
 		t.Errorf("expected trigger no error")
 	}
-	if fsm.Current() != "start" {
-		t.Error("expected state to be 'start'")
+	if fsm.Current() != LampStatus_Closed {
+		t.Error("expected state to be 'closed'")
 	}
 }
 
 func Test_Fsm_State(t *testing.T) {
-	test_Fsm_State(t, NewSafeFsm[string, string])
-	test_Fsm_State(t, NewFsm[string, string])
+	test_Fsm_State(t, NewSafeFsm[LampEvent, LampStatus])
+	test_Fsm_State(t, NewFsm[LampEvent, LampStatus])
 }
 
-func test_Fsm_State(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
+func test_Fsm_State(t *testing.T, newFsm func(initState LampStatus, ts ITransition[LampEvent, LampStatus]) IFsm[LampEvent, LampStatus]) {
 	fsm := newFsm(
-		"walking",
-		NewTransition([]Transform[string, string]{
-			{Event: "walk", Src: []string{"start"}, Dst: "walking"},
-			{Event: "look", Src: []string{"walking"}, Dst: "walking"},
+		LampStatus_Closed,
+		NewTransition([]Transform[LampEvent, LampStatus]{
+			{Event: LampEvent_Close, Src: []LampStatus{LampStatus_Opened}, Dst: LampStatus_Closed},
+			{Event: LampEvent_Look, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Closed},
 		}),
 	)
 
-	if !fsm.ContainsEvent("walk") {
-		t.Error("expected support event 'walk'")
+	if !fsm.ContainsEvent(LampEvent_Close) {
+		t.Error("expected support event 'close'")
 	}
-	if fsm.ContainsEvent("nosupport") {
-		t.Error("expected not support event 'nosupport'")
+	if fsm.ContainsEvent(LampEvent_PartialClose) {
+		t.Error("expected not support event 'partial-close'")
 	}
-	if !fsm.ContainsAllEvent("walk", "look") {
-		t.Error("expected support all event 'walk' and 'look'")
+	if !fsm.ContainsAllEvent(LampEvent_Close, LampEvent_Look) {
+		t.Error("expected support all event 'close' and 'look'")
 	}
-	if fsm.ContainsAllEvent("walk", "nosupport") {
-		t.Error("expected support all event 'walk' and 'nosupport'")
+	if fsm.ContainsAllEvent(LampEvent_Close, LampEvent_PartialClose) {
+		t.Error("expected not support all event 'close' and 'partial-close'")
 	}
 
-	if b, err := fsm.Match("start", "walking", "walk"); err != nil {
+	if b, err := fsm.Match(LampStatus_Opened, LampStatus_Closed, LampEvent_Close); err != nil {
 		t.Error("expected event src dst state match no error")
 	} else {
 		if !b {
 			t.Error("expected event src dst state match")
 		}
 	}
-
-	if b, err := fsm.Match("start", "walking", "look"); err == nil {
+	if b, err := fsm.Match(LampStatus_Opened, LampStatus_Closed, LampEvent_Look); err == nil {
 		t.Error("expected event src dst state match has error")
 	} else {
 		if b {
 			t.Error("expected event src dst state match false")
 		}
 	}
-
-	fsm.SetCurrent("start")
-	if fsm.Current() != "start" {
-		t.Error("expected state to be 'start'")
+	fsm.SetCurrent(LampStatus_Opened)
+	if fsm.Current() != LampStatus_Opened {
+		t.Error("expected state to be 'opened'")
 	}
-	if !fsm.MatchCurrentOccur("walk") {
+	if !fsm.MatchCurrentOccur(LampEvent_Close) {
 		t.Error("expected event can occur in the current state.")
 	}
 
-	err := fsm.Trigger("walk")
+	err := fsm.Trigger(LampEvent_Close)
 	if err != nil {
 		t.Error("trigger is expected no error")
 	}
-	if !fsm.Is("walking") {
-		t.Error("expected state to be 'walking'")
+	if !fsm.Is(LampStatus_Closed) {
+		t.Error("expected state to be 'closed'")
 	}
 }
 
 func Test_Fsm_Avail(t *testing.T) {
-	test_Fsm_Avail(t, NewSafeFsm[string, string])
-	test_Fsm_Avail(t, NewFsm[string, string])
+	test_Fsm_Avail(t, NewSafeFsm[LampEvent, LampStatus])
+	test_Fsm_Avail(t, NewFsm[LampEvent, LampStatus])
 }
 
-func test_Fsm_Avail(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
+func test_Fsm_Avail(t *testing.T, newFsm func(initState LampStatus, ts ITransition[LampEvent, LampStatus]) IFsm[LampEvent, LampStatus]) {
 	fsm := newFsm(
-		"closed",
-		NewTransition([]Transform[string, string]{
-			{Event: "open", Src: []string{"closed"}, Dst: "open"},
-			{Event: "close", Src: []string{"open"}, Dst: "closed"},
-			{Event: "middle", Src: []string{"closed"}, Dst: "middle"},
+		LampStatus_Closed,
+		NewTransition([]Transform[LampEvent, LampStatus]{
+			{Event: LampEvent_Open, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Opened},
+			{Event: LampEvent_Close, Src: []LampStatus{LampStatus_Opened}, Dst: LampStatus_Closed},
+			{Event: LampEvent_PartialOpen, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Intermediate},
 		}),
 	)
 	events := fsm.CurrentAvailEvents()
-	if !(slices.Contains(events, "middle") && slices.Contains(events, "open")) {
-		t.Error("expected contain [middle, open] event with current state")
+	if !(slices.Contains(events, LampEvent_Open) && slices.Contains(events, LampEvent_PartialOpen)) {
+		t.Error("expected contain [open, partial-open] event with current state")
 	}
 	sortedEvents := fsm.SortedEvents()
-	if !slices.Equal(sortedEvents, []string{"close", "middle", "open"}) {
-		t.Error("expected sort event [close, middle, open] event with current state")
+	if !slices.Equal(sortedEvents, []LampEvent{LampEvent_Close, LampEvent_Open, LampEvent_PartialOpen}) {
+		t.Error("expected sort event [close, open, partial-open] event with current state")
 	}
-	availSourceStates := fsm.AvailSourceStates("open")
-	if !slices.Contains(availSourceStates, "closed") {
+	availSourceStates := fsm.AvailSourceStates(LampEvent_PartialOpen)
+	if !slices.Contains(availSourceStates, LampStatus_Closed) {
 		t.Error("expected avail source state [closed] with the event")
 	}
-
-	if !fsm.MatchCurrentAllOccur("middle", "open") {
-		t.Error("expected contain all [middle, open] event with current state")
+	if !fsm.MatchCurrentAllOccur(LampEvent_PartialOpen, LampEvent_Open) {
+		t.Error("expected contain all [partial-open, open] event with current state")
 	}
-	if fsm.MatchCurrentAllOccur("open", "close") {
-		t.Error("expected not contain all [middle, open] event with current state")
+	if fsm.MatchCurrentAllOccur(LampEvent_PartialOpen, LampEvent_Close) {
+		t.Error("expected not contain all [partial-open, open] event with current state")
 	}
 }
-func Test_Fsm_InappropriateEvent(t *testing.T) {
-	test_Fsm_InappropriateEvent(t, NewSafeFsm[string, string])
-	test_Fsm_InappropriateEvent(t, NewFsm[string, string])
+func Test_Fsm_NonExistEvent_InappropriateEvent(t *testing.T) {
+	test_Fsm_NonExistEvent_InappropriateEvent(t, NewSafeFsm[LampEvent, LampStatus])
+	test_Fsm_NonExistEvent_InappropriateEvent(t, NewFsm[LampEvent, LampStatus])
 }
 
-func test_Fsm_InappropriateEvent(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
+func test_Fsm_NonExistEvent_InappropriateEvent(t *testing.T, newFsm func(initState LampStatus, ts ITransition[LampEvent, LampStatus]) IFsm[LampEvent, LampStatus]) {
 	fsm := newFsm(
-		"closed",
-		NewTransition([]Transform[string, string]{
-			{Event: "open", Src: []string{"closed"}, Dst: "open"},
-			{Event: "close", Src: []string{"open"}, Dst: "closed"},
+		LampStatus_Closed,
+		NewTransition([]Transform[LampEvent, LampStatus]{
+			{Event: LampEvent_Open, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Opened},
+			{Event: LampEvent_Close, Src: []LampStatus{LampStatus_Opened}, Dst: LampStatus_Closed},
 		}),
 	)
 
-	err := fsm.Trigger("close")
+	err := fsm.Trigger(LampEvent_PartialClose)
+	if err != ErrNonExistEvent {
+		t.Error("expected 'ErrNonExistEvent' with incorrect event")
+	}
+	err = fsm.Trigger(LampEvent_Close)
 	if err != ErrInappropriateEvent {
 		t.Error("expected 'ErrInappropriateEvent' with correct state and event")
 	}
 }
 
-func Test_Fsm_NonExistEvent(t *testing.T) {
-	test_Fsm_NonExistEvent(t, NewSafeFsm[string, string])
-	test_Fsm_NonExistEvent(t, NewFsm[string, string])
+func Test_Fsm_TranslateError(t *testing.T) {
+	test_Fsm_TranslateError(t, NewSafeFsm[LampEvent, LampStatus])
+	test_Fsm_TranslateError(t, NewFsm[LampEvent, LampStatus])
 }
 
-func test_Fsm_NonExistEvent(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
+func test_Fsm_TranslateError(t *testing.T, newFsm func(initState LampStatus, ts ITransition[LampEvent, LampStatus]) IFsm[LampEvent, LampStatus]) {
 	fsm := newFsm(
-		"closed",
-		NewTransition([]Transform[string, string]{
-			{Event: "open", Src: []string{"closed"}, Dst: "open"},
-			{Event: "close", Src: []string{"open"}, Dst: "closed"},
-		}),
+		LampStatus_Closed,
+		NewTransitionBuilder(
+			[]Transform[LampEvent, LampStatus]{
+				{Event: LampEvent_Open, Src: []LampStatus{LampStatus_Closed}, Dst: LampStatus_Opened},
+				{Event: LampEvent_Close, Src: []LampStatus{LampStatus_Opened}, Dst: LampStatus_Closed},
+			}).
+			TranslatorError(&testTranslatorError{}).
+			Build(),
 	)
-
-	err := fsm.Trigger("lock")
-	if err != ErrNonExistEvent {
-		t.Error("expected 'ErrNonExistEvent' with incorrect event")
+	err := fsm.Trigger(LampEvent_Close)
+	if err == nil {
+		t.Error("expected a error")
+	} else {
+		if !strings.Contains(err.Error(), "err1") {
+			t.Error("expected a error <err1>")
+		}
 	}
+	err = fsm.Trigger(LampEvent_PartialClose)
+	if err == nil {
+		t.Error("expected a error")
+	} else {
+		if !strings.Contains(err.Error(), "err2") {
+			t.Error("expected a error <err2>")
+		}
+	}
+
 }
+
 func Test_Fsm_MultipleSources(t *testing.T) {
 	testFsm_MultipleSources(t, NewSafeFsm[string, string])
 	testFsm_MultipleSources(t, NewFsm[string, string])
@@ -313,51 +344,5 @@ func test_Fsm_MultipleEvents(t *testing.T, newFsm func(initState string, ts ITra
 	}
 	if fsm.Current() != "start" {
 		t.Error("expected state to be 'start'")
-	}
-}
-
-func Test_Fsm_TranslateError(t *testing.T) {
-	test_Fsm_TranslateError(t, NewSafeFsm[string, string])
-	test_Fsm_TranslateError(t, NewFsm[string, string])
-}
-
-type testTranslatorError struct{}
-
-func (testTranslatorError) Translate(err error) error {
-	switch err {
-	case ErrInappropriateEvent:
-		return errors.New("one")
-	case ErrNonExistEvent:
-		return errors.New("two")
-	}
-	return err
-}
-
-func test_Fsm_TranslateError(t *testing.T, newFsm func(initState string, ts ITransition[string, string]) IFsm[string, string]) {
-	fsm := newFsm(
-		"closed",
-		NewTransitionBuilder(
-			[]Transform[string, string]{
-				{Event: "open", Src: []string{"closed"}, Dst: "open"},
-				{Event: "close", Src: []string{"open"}, Dst: "closed"},
-			}).
-			TranslatorError(&testTranslatorError{}).
-			Build(),
-	)
-	err := fsm.Trigger("close")
-	if err == nil {
-		t.Error("expected a error")
-	} else {
-		if !strings.Contains(err.Error(), "one") {
-			t.Error("expected a error <one>")
-		}
-	}
-	err = fsm.Trigger("nosupport")
-	if err == nil {
-		t.Error("expected a error")
-	} else {
-		if !strings.Contains(err.Error(), "two") {
-			t.Error("expected a error <two>")
-		}
 	}
 }
